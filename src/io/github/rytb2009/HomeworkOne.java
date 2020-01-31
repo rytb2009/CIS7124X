e0da68633eeaee05574fd70f0160d338aba48298
Ray <rytb2009@gmail.com>
2020-01-31 15:36:47 -0500

    Homework one commit

--- src/io/github/rytb2009/HomeworkOne.java
+++ src/io/github/rytb2009/HomeworkOne.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Main {
+public class HomeworkOne {
 
     public static void main(String[] args) {
         int gcdX = 36;
